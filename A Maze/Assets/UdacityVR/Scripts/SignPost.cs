87ffeea8796d68bdaf228aa3f720a4090f580bf8
tlozzer1 <tlozzer@gmail.com>
2019-04-13 16:35:45 -0300

    Programming SignPost behaviour (no chest opening)

--- A Maze/Assets/UdacityVR/Scripts/SignPost.cs
+++ A Maze/Assets/UdacityVR/Scripts/SignPost.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 // TODO: Get convenient access the Scene and SceneManager classes
 // Use the 'using' keyword to include the SceneManagement namespace
 
@@ -13,8 +14,10 @@ public class SignPost : MonoBehaviour {
 		// Prints to the console when the method is called
 		Debug.Log ("'SignPost.ResetScene()' was called");
 
-		// TODO: Reset the scene by getting a reference to the scene and reloading it
-		// Declare a Scene named 'scene', then use SceneManager.GetActiveScene () to get the current scene and assign it to 'scene'
-		// Use SceneManager.LoadScene() and the Scene.name property to reload the scene
+        // TODO: Reset the scene by getting a reference to the scene and reloading it
+        // Declare a Scene named 'scene', then use SceneManager.GetActiveScene () to get the current scene and assign it to 'scene'
+        Scene scene = SceneManager.GetActiveScene();
+        // Use SceneManager.LoadScene() and the Scene.name property to reload the scene
+        SceneManager.LoadScene(scene.name);
 	}
 }
